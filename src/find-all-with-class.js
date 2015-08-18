50abd00a2e36e9618eb07077fc3321022e0fb33c
Stefan Buck <stefanb@brandwatch.com>
2015-08-18 09:53:56 +0200

    Add support for multiple class names

--- src/find-all-with-class.js
+++ src/find-all-with-class.js
@@ -2,6 +2,18 @@ import React from 'react/addons';
 import findAll from './find-all';
 
 /**
+ * Returns true if the given parameter classNameList contains the
+ * given paramter className.
+ *
+ * @param  {String}  classNameList String of all class names
+ * @param  {String}  className     The class name to search for
+ * @return {Boolean}
+ */
+function hasClassName(classNameList, className) {
+  return ` ${classNameList} `.indexOf(` ${className} `) !== -1;
+}
+
+/**
  * Finds all instances of components in the tree with a class that matches
  * `className`.
  *
@@ -16,7 +28,16 @@ export default function findAllWithClass(tree, className) {
   return findAll(tree, component => {
     if (React.isValidElement(component)) {
       if(component.props.className != null) {
-        return ` ${component.props.className} `.indexOf(` ${className} `) !== -1;
+
+        if (className.includes('.')) {
+          const classNameList = className.split('.');
+
+          return classNameList.every(function(val) {
+              return hasClassName(component.props.className, val);
+          });
+        }
+
+        return hasClassName(component.props.className, className);
       }
 
       return false;
