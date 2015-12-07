c13f0bace7d36dd384019031b0eec68267c0eb03
Chris Shepherd <chris@chrisshepherd.me>
2015-12-07 17:15:57 +0000

    Forgot to add export for `findWithRef`

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import findAll from './find-all';
 import findAllWithClass from './find-all-with-class';
 import findAllWithType from './find-all-with-type';
 import findWithClass from './find-with-class';
+import findWithRef from './find-with-ref';
 import findWithType from './find-with-type';
 import getMountedInstance from './get-mounted-instance';
 import isComponentOfType from './is-component-of-type';
@@ -12,6 +13,7 @@ export default {
   findAllWithClass,
   findAllWithType,
   findWithClass,
+  findWithRef,
   findWithType,
   getMountedInstance,
   isComponentOfType,
